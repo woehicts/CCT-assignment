a561318c901618cb0014c0e8ca06c6243a78beb5
woehicts <72599907+woehicts@users.noreply.github.com>
2022-06-03 11:11:09 +0300

    Implement find_maximum_distance

--- task.py
+++ task.py
@@ -27,13 +27,27 @@ from typing import List
 def find_maximum_distance(
     number_of_cities: int, cities_with_train_station: List[int]
 ) -> int:
-    # TODO: Replace this with real implementation:
-    if number_of_cities == 3:
-        return 1
-    elif number_of_cities == 4:
-        return 3
+    """Finds the largest possible distance to the nearest train station within a list of cities, using the provided list of the train stations assigned to them.
+        
+        `number_of_cities` (int) - defines the number of cities within the list
+        `cities_with_train_station` (List[int]) - specifies the cities that have the train stations
+    """
+    if number_of_cities <= 0:
+        raise ValueError("The number of cities should not be less than one")
+    if not isinstance(cities_with_train_station, list):
+        raise TypeError("'cities_with_train_station' should be a list of numbers")
+    sorted_stations = sorted(set([city for city in cities_with_train_station if isinstance(city,int) and city >= 0 and city < number_of_cities]))
+    number_of_stations = len(sorted_stations)
+    if  number_of_stations == 0:
+        raise ValueError("Either the train stations haven't been assigned with valid city numbers or the list of stations was an empty list")
+    elif number_of_stations == 1:
+        max_interstation_half_distance = 0
     else:
-        return 2
+        max_interstation_half_distance = max([(station2 - station1)// 2 for station1, station2 in zip(sorted_stations[:-1],sorted_stations[1:])])
+    first_station = sorted_stations[0]
+    last_station = sorted_stations[-1]
+    last_city = number_of_cities - 1
+    return max(first_station, last_city - last_station, max_interstation_half_distance)
 
 
 if __name__ == "__main__":
